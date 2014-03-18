8b9867a29598b104f98ed271dfcd2c9215f96043
kwaters12 <kellywaters@gmail.com>
2014-03-17 20:38:48 -0700

    Fixing s3 and Heroku integration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,8 @@ Waterdesign::Application.configure do
 
   # Integrating Paperclip and Amazon S3
   config.paperclip_defaults = {
+  Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
   :storage => :s3,
   :s3_credentials => {
     :bucket => ENV['waterdesign.ca'],
